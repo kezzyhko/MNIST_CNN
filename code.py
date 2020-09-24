c7a3e1b53447100509d0ceac404ec31abff8664c
Sergey Semushin <s.semushin@innopolis.university>
2020-09-24 17:29:16 +0300

    GSD momentum = 0 | 0.8081

--- code.py
+++ code.py
@@ -66,7 +66,7 @@ model.add(Dense(512, activation='relu'))
 model.add(Dense(512, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(optimizer=SGD(lr=0.01, momentum=0.9), loss='categorical_crossentropy', metrics=[Recall(), Precision()])
+model.compile(optimizer=SGD(lr=0.01), loss='categorical_crossentropy', metrics=[Recall(), Precision()])
 
 
 
