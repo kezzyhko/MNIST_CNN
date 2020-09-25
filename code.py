26b4cddfffb1cd7c419ea384d62d7f2bfc5c1809
Sergey Semushin <s.semushin@innopolis.university>
2020-09-25 22:26:03 +0300

    Added BatchNormalisation as the first layer | 0.8006

--- code.py
+++ code.py
@@ -47,33 +47,20 @@ generator = ImageDataGenerator(
 model = Sequential()
 model.add(BatchNormalization(input_shape=(28, 28, 1)))
 
-model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same'))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same', input_shape=(28, 28, 1)))
 model.add(BatchNormalization())
-model.add(Dropout(0.3))
 model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
 
 model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
-model.add(Dropout(0.3))
 
 model.add(Conv2D(filters=256, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
-model.add(Dropout(0.3))
-
-model.add(Conv2D(filters=512, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(Conv2D(filters=512, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
-model.add(Dropout(0.3))
 
 model.add(Flatten())
-model.add(Dense(4096, activation='relu'))
-model.add(Dropout(0.3))
 model.add(Dense(1024, activation='relu'))
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.3))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer=SGD(lr=0.01, momentum=0.9), loss='categorical_crossentropy', metrics=[Recall(), Precision()])
