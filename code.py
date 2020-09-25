2a51412133a6df737034ef5cbbf6a61160cae589
Sergey Semushin <s.semushin@innopolis.university>
2020-09-25 23:07:59 +0300

    Dropout, more dense perceptrons and +1 conv layer | 0.8307

--- code.py
+++ code.py
@@ -56,6 +56,9 @@ model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
 model.add(Conv2D(filters=256, kernel_size=(3,3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
 
+model.add(Conv2D(filters=512, kernel_size=(3,3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='same'))
+
 model.add(Flatten())
 model.add(Dense(4096, activation='relu'))
 model.add(Dropout(0.2))
