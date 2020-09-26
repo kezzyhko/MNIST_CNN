f5e8ed04c9d1d8d742089fb5947b470ffa54e6c4
Sergey Semushin <s.semushin@innopolis.university>
2020-09-26 12:23:08 +0300

    No padding, strides=1 | 0.84

--- code.py
+++ code.py
@@ -46,17 +46,17 @@ generator = ImageDataGenerator(
 
 model = Sequential()
 
-model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same', input_shape=(28, 28, 1)))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='valid', input_shape=(28, 28, 1)))
 model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2,2), strides = 2))
+model.add(MaxPooling2D(pool_size=(2,2), strides = 2, padding='valid'))
 
-model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2,2), strides = 2))
+model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='valid'))
+model.add(MaxPooling2D(pool_size=(2,2), strides = 1, padding='valid'))
 
-model.add(Conv2D(filters=256, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2,2), strides = 2))
+model.add(Conv2D(filters=256, kernel_size=(3,3), activation='relu', padding='valid'))
+model.add(MaxPooling2D(pool_size=(2,2), strides = 1, padding='valid'))
 
-model.add(Conv2D(filters=512, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(filters=512, kernel_size=(3,3), activation='relu', padding='valid'))
 
 model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
