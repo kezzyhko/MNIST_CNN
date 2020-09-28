0d693cbf489ef8b1bbf814994998f23735c088e9
Sergey Semushin <s.semushin@innopolis.university>
2020-09-28 14:50:28 +0300

    One more batch normalization | 0.8743

--- code.py
+++ code.py
@@ -55,6 +55,7 @@ model.add(MaxPooling2D(pool_size=(2,2), strides = 2))
 model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same'))
 
 model.add(Conv2D(filters=256, kernel_size=(3,3), activation='relu', padding='same'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides = 2))
 
 model.add(Conv2D(filters=512, kernel_size=(3,3), activation='relu'))
@@ -78,7 +79,7 @@ model.fit(
     generator.flow(x, to_categorical(y), batch_size = 128),
     validation_data = generator.flow(x, to_categorical(y), batch_size = 128, subset = 'validation'),
     steps_per_epoch = len(x) / 128,
-    epochs = 250
+    epochs = 15
 )
 
 
